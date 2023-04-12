f32ca592447b41015c013ccc4580b9f84053d6a7
jogayeon <jogayeon0113@gmail.com>
2023-04-12 20:06:09 +0900

    login-3

--- app/src/main/java/com/example/gympti/activity/MainActivity.kt
+++ app/src/main/java/com/example/gympti/activity/MainActivity.kt
@@ -8,16 +8,9 @@ import android.widget.Button
 import com.example.gympti.login.Fragment.LoginFragment
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
 
-        var btn : Button = findViewById(R.id.btn1)
-
-        btn.setOnClickListener{
-            val intent = Intent(this, LoginFragment::class.java)
-            startActivity(intent)
-        }
     }
 }
--- app/src/main/java/com/example/gympti/start/Fragment/StartFragment.kt
+++ app/src/main/java/com/example/gympti/start/Fragment/StartFragment.kt
@@ -1,21 +1,14 @@
 package com.example.gympti
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.gympti.login.Fragment.LoginFragment
-import com.example.gympti.signup.Fragment.JoinPwFragment
 
 class StartFragment : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_start)
+
 
 
 
